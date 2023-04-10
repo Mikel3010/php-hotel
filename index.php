ad750ebf822c0b96e830c6e070b472f2c9f0f574
Mikel3010 <mikelgremi30@gmail.com>
2023-04-10 22:22:09 +0200

    aggiunta bootstrap

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $hotels = [
 
     [
@@ -36,29 +36,46 @@ $hotels = [
         'vote' => 2,
         'distance_to_center' => 50
     ],
-]    
+];
+
 
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
+
 <body>
     <h1>Elenco Hotel</h1>
-    <?php foreach ($hotels as $hotel) {?>
-        <p>
-            <strong>Nome:</strong><?php echo $hotel['name'];?><br>
-            <strong>Descrizione:</strong><?php echo $hotel['description'];?><br>
-            <strong>Parcheggio:</strong><?php echo $hotel['parking'];?><br>
-            <strong>Voto:</strong><?php echo $hotel['vote'];?><br>
-            <strong>Distanza dal centro:</strong><?php echo $hotel['distance_to_center'];?><br>
-        </p>
-        <?php }?>
-    
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">Voto</th>
+                <th scope="col">Distanza dal centro</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($hotels as $hotel) : ?>
+                <tr>
+                    <td><?php echo $hotel['name'] ?></td>
+                    <td><?php echo $hotel['description'] ?></td>
+                    <td><?php echo $hotel['parking'] ?></td>
+                    <td><?php echo $hotel['vote'] ?></td>
+                    <td><?php echo $hotel['distance_to_center'] ?></td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
